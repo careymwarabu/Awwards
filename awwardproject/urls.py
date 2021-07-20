71f1bd8e2290b8c4bc213098a009eaf92c9ee733
careymwarabu <careyakmwarabu@gmail.com>
2021-07-20 03:17:00 +0300

    update awwardproject/urls.py

--- awwardproject/urls.py
+++ awwardproject/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('awwardsapp.urls')),
+
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/'),
+        name='django_registration_register'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('logout/',auth_views.LogoutView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
 ]
